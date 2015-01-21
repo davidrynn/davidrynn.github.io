5f8f38c118f56b931d616646e4f8343a41c3950a
davidrynn <davidrynn@gmail.com>
2015-01-21 13:28:37 -0500

    added scrolling functionality with text for menu.

--- alt/inc/footer.php
+++ alt/inc/footer.php
@@ -50,24 +50,16 @@
             if( classie.has( container, 'contain--open' ) ) {
                 classie.remove( container, 'contain--open' );
                 classie.remove( trigger, 'trigger--active' );
-                window.addEventListener( 'scroll', noscroll );
+
             }
             else {
                 classie.add( container, 'contain--open' );
                 classie.add( trigger, 'trigger--active' );
-                window.removeEventListener( 'scroll', noscroll );
+
             }
         }
  
-        function noscroll() {
-            window.scrollTo( 0, 0 );
-        }
- 
-        // reset scrolling position
-        document.body.scrollTop = document.documentElement.scrollTop = 0;
- 
-        // disable scrolling
-        window.addEventListener( 'scroll', noscroll );
+
  
         trigger.addEventListener( 'click', toggleContent );
 
