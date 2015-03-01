7c7d849941fa0cff1d3245e38a66393fa8fd3989
davidrynn <davidrynn@gmail.com>
2015-03-01 18:53:28 -0500

    made map full screen

--- map/index.php
+++ map/index.php
@@ -7,6 +7,6 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	<iframe width="500" height="300" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?q=select+col5+from+1H6Ix9M2sQkeeHuT9B-SUYmR3ZdUBsBeq8E4KjOHP&amp;viz=MAP&amp;h=false&amp;lat=40.71427816831297&amp;lng=-73.95819652938845&amp;t=1&amp;z=16&amp;l=col5&amp;y=2&amp;tmplt=2&amp;hml=GEOCODABLE"></iframe>
+	<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?q=select+col5+from+1H6Ix9M2sQkeeHuT9B-SUYmR3ZdUBsBeq8E4KjOHP&amp;viz=MAP&amp;h=false&amp;lat=40.71427816831297&amp;lng=-73.95819652938845&amp;t=1&amp;z=16&amp;l=col5&amp;y=2&amp;tmplt=2&amp;hml=GEOCODABLE"></iframe>
 </body>
 </html>
