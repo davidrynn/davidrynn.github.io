35130dc8b989c77fc384fcfcb94940a837092c9a
davidrynn <davidrynn@gmail.com>
2015-03-01 18:47:28 -0500

    added beta map

---
+++ map/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Rental possibilities</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<iframe width="500" height="300" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?q=select+col5+from+1H6Ix9M2sQkeeHuT9B-SUYmR3ZdUBsBeq8E4KjOHP&amp;viz=MAP&amp;h=false&amp;lat=40.71427816831297&amp;lng=-73.95819652938845&amp;t=1&amp;z=16&amp;l=col5&amp;y=2&amp;tmplt=2&amp;hml=GEOCODABLE"></iframe>
+</body>
+</html>
