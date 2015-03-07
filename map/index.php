778bc72fc5ae2269a840f7a4989acc4a9c2f0fcb
davidrynn <davidrynn@gmail.com>
2015-03-07 14:01:07 -0500

    Revert "changed to premade google code"
    This reverts commit 93c02957dc15dbad2bde8d9ba89cb5074714c1ec.

--- map/index.php
+++ map/index.php
@@ -1,74 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport"></meta>
-<title>2013BrooklynBldgs - Google Fusion Tables</title>
-<style type="text/css">
-html, body, #googft-mapCanvas {
-  height: 300px;
-  margin: 0;
-  padding: 0;
-  width: 500px;
-}
-</style>
-
-<script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false&amp;v=3"></script>
-
-<script type="text/javascript">
-  function initialize() {
-    google.maps.visualRefresh = true;
-    var isMobile = (navigator.userAgent.toLowerCase().indexOf('android') > -1) ||
-      (navigator.userAgent.match(/(iPod|iPhone|iPad|BlackBerry|Windows Phone|iemobile)/));
-    if (isMobile) {
-      var viewport = document.querySelector("meta[name=viewport]");
-      viewport.setAttribute('content', 'initial-scale=1.0, user-scalable=no');
-    }
-    var mapDiv = document.getElementById('googft-mapCanvas');
-    mapDiv.style.width = isMobile ? '100%' : '500px';
-    mapDiv.style.height = isMobile ? '100%' : '300px';
-    var map = new google.maps.Map(mapDiv, {
-      center: new google.maps.LatLng(40.71044941153745, -73.9346639894776),
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    layer = new google.maps.FusionTablesLayer({
-      map: map,
-      heatmap: { enabled: false },
-      query: {
-        select: "col5",
-        from: "1H6Ix9M2sQkeeHuT9B-SUYmR3ZdUBsBeq8E4KjOHP",
-        where: ""
-      },
-      options: {
-        styleId: 2,
-        templateId: 2
-      }
-    });
-
-    if (isMobile) {
-      var legend = document.getElementById('googft-legend');
-      var legendOpenButton = document.getElementById('googft-legend-open');
-      var legendCloseButton = document.getElementById('googft-legend-close');
-      legend.style.display = 'none';
-      legendOpenButton.style.display = 'block';
-      legendCloseButton.style.display = 'block';
-      legendOpenButton.onclick = function() {
-        legend.style.display = 'block';
-        legendOpenButton.style.display = 'none';
-      }
-      legendCloseButton.onclick = function() {
-        legend.style.display = 'none';
-        legendOpenButton.style.display = 'block';
-      }
-    }
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Rental possibilities</title>
+	<link rel="stylesheet" href="">
 </head>
-
 <body>
-  <div id="googft-mapCanvas"></div>
+	<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://www.google.com/fusiontables/embedviz?q=select+col5+from+1H6Ix9M2sQkeeHuT9B-SUYmR3ZdUBsBeq8E4KjOHP&amp;viz=MAP&amp;h=false&amp;lat=40.71427816831297&amp;lng=-73.95819652938845&amp;t=1&amp;z=16&amp;l=col5&amp;y=2&amp;tmplt=2&amp;hml=GEOCODABLE"></iframe>
 </body>
 </html>
