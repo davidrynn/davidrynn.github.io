b91660d6264488cb93eb64c18d218d17fc7516e7
davidrynn <davidrynn@gmail.com>
2015-08-19 09:55:14 -0400

    quick edits

--- web_development/index.php
+++ web_development/index.php
@@ -4,59 +4,62 @@ require_once('../inc/config.php');
 $pageTitle = "Coding Experience";
 $page = "web_development";
 include(ROOT_PATH . 'inc/header.php');
- ?>
+?>
 
 <div id="contain" class="contain">
 	
-<div class="<?php echo $page ?> intro">
-	<h2>Web Development</h2>
-	<div class="container">
-		<div class="bs-example" data-example-id="simple-table">
-    <table class="table table-condensed">
-      <caption><h3><i class="fa fa-pencil fa-2x"></i>Skills</h3></caption>
-      <thead>
-        <tr>
-          <th>Languages</th>
-          <th>Frameworks/Libraries</th>
-          <th>Version Control/Deployment</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Objective C</td>
-        </tr>
-        <tr>
-          <td>Swift</td>
-        </tr>
-        <tr>
-          <td>Javascript</td>
-          <td>jQuery</td>
-          <td>Git</td>
-        </tr>
-        <tr>
-          <td>Java</td>
-          <td>Cocoa</td>
-          <td>AWS</td>
-        </tr>
-        <tr>
-          <td>HTML</td>
-          <td></td>
-          <td>Heroku</td>
-        </tr>
-        <tr>
-          <td>CSS</td>
-          <td></td>
-          <td>/td>
-        </tr>
-        <tr>
-          <td>PHP</td>
-          <td></td>
-          <td></td>
-        </tr>
-  
-      </tbody>
-    </table>
-  </div><!-- /example -->
+  <div class="<?php echo $page ?> intro">
+    <h2>Code Development - under construction...</h2>
+   <div class="container">
+    <div class="bs-example" data-example-id="simple-table">
+      <table class="table table-condensed">
+        <caption><h3><i class="fa fa-pencil fa-2x"></i>Skills</h3></caption>
+        <thead>
+          <tr>
+            <th>Languages</th>
+            <th>Frameworks/Libraries</th>
+            <th>Version Control/Deployment</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Objective C</td>
+            <td>jQuery</td>
+            <td>Git</td>
+          </tr>
+          <tr>
+            <td>Swift</td>
+            <td>Cocoa</td>
+            <td>AWS</td>
+          </tr>
+          <tr>
+            <td>Javascript</td>
+            <td></td>
+            <td>Heroku</td>
+
+          </tr>
+          <tr>
+            <td>Java</td>
+
+          </tr>
+          <tr>
+            <td>HTML</td>
+
+          </tr>
+          <tr>
+            <td>CSS</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>PHP</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div><!-- /example -->
 
     <div class="row webDev">
       <h3><i class="fa fa-cogs fa-2x"></i>Projects</h3>
@@ -67,30 +70,30 @@ include(ROOT_PATH . 'inc/header.php');
       </div>
     </div>
 
-		<div class="row webDev">
-			<div class="col-md-6">
-				<a href="http://www.wordatic.com" target="_blank"><img src="<?php echo BASE_URL; ?>img/wordatic.gif" class="img-responsive" alt="Wordatic game"></a>
-			</div>
-			<div class="col-md-6">WORDATIC: One page app launched from Heroku.  Social word-game/story-builder.
-			</div>
-		</div>
+    <div class="row webDev">
+     <div class="col-md-6">
+      <a href="http://www.wordatic.com" target="_blank"><img src="<?php echo BASE_URL; ?>img/wordatic.gif" class="img-responsive" alt="Wordatic game"></a>
+    </div>
+    <div class="col-md-6">WORDATIC: One page app launched from Heroku.  Social word-game/story-builder.
+    </div>
+  </div>
 
-		<div class="row webDev">
-			<div class="col-md-6">
-				<a href="#"><img src="<?php echo BASE_URL; ?>img/semRater.gif" class="img-responsive" alt="Seminar Rater RoR"></a>
-			</div>
-			<div class="col-md-6"><p class="text-center">SEMINAR RATER: Ruby on Rails 4.x online database app utility for actors (in development)</p>
-			</div>
-		</div>
+  <div class="row webDev">
+   <div class="col-md-6">
+    <a href="#"><img src="<?php echo BASE_URL; ?>img/semRater.gif" class="img-responsive" alt="Seminar Rater RoR"></a>
+  </div>
+  <div class="col-md-6"><p class="text-center">SEMINAR RATER: Ruby on Rails 4.x online database app utility for actors (in development)</p>
+  </div>
+</div>
 
-	    <div id="canvasDiv" class="row">
-	    	<div class="col-md-6">
-	    		<p></p>
-	    		
-	    	</div>
+<div id="canvasDiv" class="row">
+  <div class="col-md-6">
+   <p></p>
 
-	    </div>
-	</div><!-- /container -->
+ </div>
+
+</div>
+</div><!-- /container -->
 
 <?php include(ROOT_PATH . 'inc/footer.php'); ?>
 </body>
